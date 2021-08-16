d60b2c232fff6cea864f49bdc28e58e905c3124e
saylesc <cory.sayles@gmail.com>
2021-08-15 21:42:37 -0400

    Updated Auth env vars

--- auth/auth.py
+++ auth/auth.py
@@ -9,9 +9,9 @@ from urllib.request import urlopen
 # AUTH0_DOMAIN = 'fsnd-saylesc.us.auth0.com'
 # ALGORITHMS = ['RS256']
 # API_AUDIENCE = 'fsnd-surf-events'
-AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
-ALGORITHMS = os.environ.get['ALGORITHMS']
 API_AUDIENCE = os.environ.get('API_AUDIENCE')
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = [os.environ.get('ALGORITHMS')]
 AUTHORIZATION = 'Authorization'
 BEARER = 'bearer'
 
--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-export FLASK_APP=flaskrsurf
+export FLASK_APP=flaskrsurf.py
 export FLASK_ENV=development
 export AUTH0_DOMAIN='fsnd-saylesc.us.auth0.com'
-export ALGORITHMS=['RS256']
+export ALGORITHMS='RS256'
 export API_AUDIENCE='fsnd-surf-events'
+export DATABASE_URL=postgresql://postgres:postgres@localhost:5432/surf_events
