cd4c0dd4a0c459bbf83147d05e7adca68e0fbf03
saylesc <cory.sayles@gmail.com>
2021-08-15 21:16:27 -0400

    Updated auth vars

--- auth/auth.py
+++ auth/auth.py
@@ -5,11 +5,16 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'fsnd-saylesc.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'fsnd-surf-events'
+# AUTH0_DOMAIN = 'fsnd-saylesc.us.auth0.com'
+# ALGORITHMS = ['RS256']
+# API_AUDIENCE = 'fsnd-surf-events'
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get['ALGORITHMS']
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 AUTHORIZATION = 'Authorization'
 BEARER = 'bearer'
+
+
 
 # AuthError Exception
 '''
--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
 export FLASK_APP=flaskrsurf
 export FLASK_ENV=development
+export AUTH0_DOMAIN='fsnd-saylesc.us.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='fsnd-surf-events'
