4e8ef939e2b320a002bfc9936f6ad6edc719fe4c
saylesc <cory.sayles@gmail.com>
2021-10-24 18:49:03 -0400

    Removed DB URL

--- setup.sh
+++ setup.sh
@@ -3,6 +3,3 @@ export FLASK_ENV=development
 export AUTH0_DOMAIN='fsnd-saylesc.us.auth0.com'
 export ALGORITHMS='RS256'
 export API_AUDIENCE='fsnd-surf-events'
-# export DATABASE_URL=postgresql://postgres:postgres@localhost:5432/surf_events
-export DATABASE_URL=postgresql://postgres:postgres@localhost:5432/surf_events_test
-# export DATABASE_URL=postgresql://zvvpwzncgnygdf:73cebe5f3178547bb39ec8e110b8ca80770723ccef8d912f53adadfe5002b392@ec2-23-21-4-7.compute-1.amazonaws.com:5432/d8fa8ad78jf0cl
